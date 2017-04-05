7b89bbf601863809423181cd0298cfd8f5e31554
jingliu <jingliu8611@gmail.com>
2017-04-05 01:07:38 -0700

    moved app-wide services and modules into core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, APP_INITIALIZER} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
-
 import {AppComponent} from './app.component';
 import {AngularFireModule} from 'angularfire2';
-import {AuthService} from './shared/services/auth.service';
-import {NgRedux, NgReduxModule} from 'ng2-redux';
-
-import {rootReducer, INITIAL_STATE} from './store';
 import {CoreModule} from './core/core.module';
-import {FirebaseAdapter} from './shared/adapters/firebase.adapter';
-import {AppRoutingModule} from './app-routing.module';
-import {InitService} from "./shared/services/init.service";
 
 export const firebaseConfig = {
     apiKey: 'AIzaSyCzCGkdxYH6rzfEuVwrTD2pLKNcJDgYbzw',
@@ -21,10 +13,6 @@ export const firebaseConfig = {
     messagingSenderId: '470792256226'
 };
 
-export function initFactory (initService: InitService) {
-    return () => initService.load();
-}
-
 @NgModule({
     declarations: [
         AppComponent
@@ -32,24 +20,10 @@ export function initFactory (initService: InitService) {
     imports: [
         BrowserModule,
         HttpModule,
-        NgReduxModule,
-        AppRoutingModule,
         CoreModule,
         AngularFireModule.initializeApp(firebaseConfig)
-    ],
-    providers: [
-        InitService,
-        AuthService,
-        FirebaseAdapter,
-        { provide: APP_INITIALIZER,
-            useFactory: initFactory,
-            deps: [InitService],
-            multi: true }
     ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
-    constructor(ngRedux: NgRedux<any>) {
-        ngRedux.configureStore(rootReducer, INITIAL_STATE);
-    }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,19 @@
-import {NgModule} from '@angular/core';
+import {NgModule, APP_INITIALIZER} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {FooterComponent} from './footer/footer.component';
 import {LoginComponent} from './login/login.component';
 import {CommonModule} from '@angular/common';
 import {AppRoutingModule} from '../app-routing.module';
+import {NgReduxModule, NgRedux} from 'ng2-redux';
+import {AuthService} from '../shared/services/auth.service';
+import {FirebaseAdapter} from '../shared/adapters/firebase.adapter';
+import {InitService} from '../shared/services/init.service';
+import {rootReducer, INITIAL_STATE} from '../store';
+
+export function initFactory (initService: InitService) {
+    return () => initService.load();
+}
 
 @NgModule({
     declarations: [
@@ -17,11 +26,24 @@ import {AppRoutingModule} from '../app-routing.module';
         CommonModule,
         AppRoutingModule
     ],
+    providers: [
+        AuthService,
+        FirebaseAdapter,
+        InitService,
+        { provide: APP_INITIALIZER,
+            useFactory: initFactory,
+            deps: [InitService],
+            multi: true }
+    ],
     exports: [
         AppRoutingModule,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        NgReduxModule
     ]
 })
 export class CoreModule {
+    constructor(ngRedux: NgRedux<any>) {
+        ngRedux.configureStore(rootReducer, INITIAL_STATE);
+    }
 }
