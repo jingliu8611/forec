0b3a1145426141a677b7b990ec5fdb0e38278d29
jingliu <jingliu8611@gmail.com>
2017-04-05 01:11:08 -0700

    moved firebase config constants to config file, which will hold all the config constants of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,7 @@ import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
 import {AngularFireModule} from 'angularfire2';
 import {CoreModule} from './core/core.module';
-
-export const firebaseConfig = {
-    apiKey: 'AIzaSyCzCGkdxYH6rzfEuVwrTD2pLKNcJDgYbzw',
-    authDomain: 'forec-bd810.firebaseapp.com',
-    databaseURL: 'https://forec-bd810.firebaseio.com',
-    storageBucket: 'forec-bd810.appspot.com',
-    messagingSenderId: '470792256226'
-};
+import {firebaseConfig} from './shared/constants/config';
 
 @NgModule({
     declarations: [
---
+++ src/app/shared/constants/config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCzCGkdxYH6rzfEuVwrTD2pLKNcJDgYbzw',
+    authDomain: 'forec-bd810.firebaseapp.com',
+    databaseURL: 'https://forec-bd810.firebaseio.com',
+    storageBucket: 'forec-bd810.appspot.com',
+    messagingSenderId: '470792256226'
+};
