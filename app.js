e3a2d993cf570be46e2c1697856b1add3e0a2208
jingliu <jingliu8611@gmail.com>
2018-02-13 22:50:20 -0800

    chore: heroku deploy update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/src'));
-app.listen(process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
